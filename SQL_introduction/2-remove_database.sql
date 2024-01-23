dbc9d9f9c182bc5d89cd897a9d4a4f8d4852ac03
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 16:44:57 +0300

    requirement error fix

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,16 +1,16 @@
-#!/bin/bash
+-- #!/bin/bash
 
-# MySQL credentials
+-- MySQL credentials
 MYSQL_USER="your_mysql_user"
 MYSQL_PASSWORD="your_mysql_password"
 
-# Database name to be deleted
+-- Database name to be deleted
 DB_NAME="hbtn_0c_0"
 
-# MySQL command to drop the database
+-- MySQL command to drop the database
 mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "DROP DATABASE IF EXISTS $DB_NAME;"
 
-# Check the exit status of the MySQL command
+--Check the exit status of the MySQL command
 if [ $? -eq 0 ]; then
     echo "Database $DB_NAME deleted successfully."
 else
