290125d2e84133ee7e00d02b89d99e801e6bb640
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 19:19:03 +0300

    requirement error fix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---#!/bin/bash
+#!/bin/bash
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 1 ]; then
@@ -13,23 +13,16 @@ db_name="$1"
 mysql_user="your_mysql_username"
 mysql_password="your_mysql_password"
 
-# Create table if not exists
+# Create table and insert records
 mysql -u "$mysql_user" -p"$mysql_password" "$db_name" <<EOF
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-EOF
 
-# Insert records
-mysql -u "$mysql_user" -p"$mysql_password" "$db_name" <<EOF
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
 EOF
-
-# Display records
-echo "Records in second_table:"
-mysql -u "$mysql_user" -p"$mysql_password" "$db_name" -e "SELECT * FROM second_table;"
