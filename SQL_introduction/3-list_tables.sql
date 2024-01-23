2e7382c3ae66984de2528e0eddd7713adbcc21e6
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 20:18:51 +0300

    requirement error fix

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,21 +1,21 @@
 -- create_database_and_tables.sql
 
 -- Create a new database
-CREATE DATABASE IF NOT EXISTS `mysql`;
-USE `mysql`;
+CREATE DATABASE IF NOT EXISTS `holbteron_0`;
+USE `holbteron_0`;
 
 -- Create three tables
-CREATE TABLE IF NOT EXISTS `holbteron_0` (
+CREATE TABLE IF NOT EXISTS `holbteron_1` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `holbteron_1` (
+CREATE TABLE IF NOT EXISTS `holbteron_2` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `description` TEXT
 );
 
-CREATE TABLE IF NOT EXISTS `holbteron_2` (
+CREATE TABLE IF NOT EXISTS `hbtn_test_db_0` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `value` DECIMAL(10, 2) NOT NULL
 );
