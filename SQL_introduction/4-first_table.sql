488b92f8da4ff164e467cd55e3b1948fcefc6ffd
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 17:10:00 +0300

    requirement error fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,7 +2,19 @@
 
 USE `hbtn_0c_0`;
 
-CREATE TABLE IF NOT EXISTS `first_table` (
-    `id` INT,
-    `name` VARCHAR(256)
-);
+-- Check if the table exists
+IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table') THEN
+
+    -- Table doesn't exist, so create it
+    CREATE TABLE `first_table` (
+        `id` INT,
+        `name` VARCHAR(256)
+    );
+
+    SELECT 'Table created' AS output_message;
+
+ELSE
+    -- Table already exists
+    SELECT 'Table already exists' AS output_message;
+
+END IF;
