1c097a1c2be0b0d59a712d38c8b2a92ac9a26fd6
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 17:42:04 +0300

    requirement error fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -9,23 +9,3 @@ CREATE TABLE IF NOT EXISTS `first_table` (
 );
 
 
--- Set the database name based on the argument passed to the mysql command
-USE `?`;
-
--- Check if the table already exists
-SET @tableExists = (
-    SELECT COUNT(*)
-    FROM information_schema.tables
-    WHERE table_schema = DATABASE() AND table_name = 'first_table'
-);
-
--- If the table doesn't exist, create it
-IF @tableExists = 0 THEN
-    CREATE TABLE `first_table` (
-        `id` INT,
-        `name` VARCHAR(256)
-    );
-    SELECT 'Table created' AS output_message;
-ELSE
-    SELECT 'Table already exists' AS output_message;
-END IF;
