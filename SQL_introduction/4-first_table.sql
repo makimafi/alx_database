fa0141dedc15a4a47254cf66b2545b5b10d28e84
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 21:53:49 +0300

    requirement error fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,10 +1,12 @@
--- 4-first_table.sql
+-- create_first_table.sql
 
--- Check if the table already exists
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+-- Check if the table already exists in the current database
+SET @table_exists = (SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'first_table');
 
--- Informative message
-SELECT 'Table first_table created or already exists.' AS 'Result';
+-- Create the table only if it doesn't exist
+DO IF @table_exists = 0 THEN
+    CREATE TABLE first_table (
+        id INT,
+        name VARCHAR(256)
+    );
+END IF;
