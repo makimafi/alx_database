fde6e127106a6e66d25a0e07442da6a450eefbcb
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 21:49:30 +0300

    requirement error fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,11 +1,13 @@
 -- 4-first_table.sql
 
+-- Use the specified database
+USE hbtn_0c_0;
 
-USE `hbtn_0c_0`;
-
-CREATE TABLE IF NOT EXISTS `first_table` (
-    `id` INT,
-    `name` VARCHAR(256)
+-- Check if the table already exists
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
 
-
+-- Informative message
+SELECT 'Table first_table created or already exists.' AS 'Result';
