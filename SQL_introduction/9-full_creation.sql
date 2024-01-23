471305d4c48fe43fa287b2e01ae98ed40cf70d4f
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 19:17:18 +0300

    requirement error fix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -13,16 +13,23 @@ db_name="$1"
 mysql_user="your_mysql_username"
 mysql_password="your_mysql_password"
 
-# Create table and insert records
+# Create table if not exists
 mysql -u "$mysql_user" -p"$mysql_password" "$db_name" <<EOF
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
+EOF
 
+# Insert records
+mysql -u "$mysql_user" -p"$mysql_password" "$db_name" <<EOF
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
 EOF
+
+# Display records
+echo "Records in second_table:"
+mysql -u "$mysql_user" -p"$mysql_password" "$db_name" -e "SELECT * FROM second_table;"
