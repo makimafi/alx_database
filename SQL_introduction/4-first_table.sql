3804223343703da3096a53abd6a4891c214ee1b4
maleda mesfin <markanmesfin555@gmail.com>
2024-01-23 17:15:18 +0300

    requirement error fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS `first_table` (
     `id` INT,
     `name` VARCHAR(256)
 );
+
+ SELECT 'Table created' AS output_message;
+
+ELSE
+    -- Table already exists
+    SELECT 'Table already exists' AS output_message;
+
+END IF;
