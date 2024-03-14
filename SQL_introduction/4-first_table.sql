9b68708e2375d5ea05c6ac0379ccabc90576c41c
maleda mesfin <markanmesfin555@gmail.com>
2024-03-14 11:49:34 +0300

    new file

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,9 @@
--- 4-first_table.sql
-
 USE `hbtn_0c_0`;
 
+-- Check if the table exists before attempting to create it
+SELECT COUNT(id) FROM first_table;
+
+-- Create the table only if it doesn't exist
 CREATE TABLE IF NOT EXISTS `first_table` (
     `id` INT,
     `name` VARCHAR(256)
