8e34897b0b87b96670b5f314c05c456b0a4b81ab
maleda mesfin <markanmesfin555@gmail.com>
2024-03-14 11:51:13 +0300

    new file

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,10 +1,18 @@
 USE `hbtn_0c_0`;
 
--- Check if the table exists before attempting to create it
-SELECT COUNT(id) FROM first_table;
-
--- Create the table only if it doesn't exist
-CREATE TABLE IF NOT EXISTS `first_table` (
+-- Attempt to create the table
+CREATE TABLE `first_table` (
     `id` INT,
     `name` VARCHAR(256)
 );
+
+-- Check for errors
+SELECT
+    CASE
+        WHEN errno = 1050 THEN 'Table already exists'
+        ELSE 'Unknown error'
+    END AS message
+FROM
+    mysql.proc
+WHERE
+    db = DATABASE() AND name = 'first_table';
